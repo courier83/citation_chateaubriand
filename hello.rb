1fb0d9dc21fb800a1b39b3ad9a0380be011bb0ef
courier83 <courier83@me.com>
2014-10-26 10:10:32 +0100

    améliorations

--- hello.rb
+++ hello.rb
@@ -5,4 +5,5 @@ puts " ton âge ???"
 age = gets.chomp
 naissance = annee.to_i - age.to_i
 puts "tu as " + age + " années, tu es donc né en " + naissance.to_s
-puts naissance.to_s.reverse * age.to_i
+puts "je vais donc écrire " + age + " fois ton année de naissance qui est: " + naissance.to_s + "."
+puts naissance.to_s * age.to_i
