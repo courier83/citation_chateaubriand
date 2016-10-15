d84ec834330280f6bafa7b76b575cac2cfce7873
courier83 <courier83@me.com>
2016-10-15 18:34:47 +0200

    Delete hello.rb

--- hello.rb
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/ruby
-#apprendre ruby.....y'a du taf !!
-annee = "2014"
-puts " ton âge ???"
-age = gets.chomp
-naissance = annee.to_i - age.to_i
-puts "tu as " + age + " années, tu es donc né en " + naissance.to_s
-puts "je vais donc écrire " + age + " fois ton année de naissance qui est: " + naissance.to_s + "."
-puts naissance.to_s * age.to_i
